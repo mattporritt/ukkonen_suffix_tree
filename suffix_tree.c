83489364d1caef33cd1e051f824e44907f7f4bf8
Matt Porritt <mattp@catalyst-au.net>
2016-05-13 09:22:11 +1000

    add initial code to support dynamic arrays

--- suffix_tree.c
+++ suffix_tree.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include "suffix_tree.h"
+#include "vector.h"
 
 unsigned char print_enabled = 0;
 unsigned char *text = NULL; //Input string 1 (for suffix tree)
@@ -609,4 +610,55 @@ void buildString(char** current_text, const char *new_text){
         *current_text = realloc(*current_text, (new_len + current_len));
         strncat(*current_text, new_text, new_len);
 
+}
+
+/*
+ * Vector (Dynamic Array) helper functions
+ */
+
+void vector_init(Vector *vector) {
+  // initialize size and capacity
+  vector->size = 0;
+  vector->capacity = VECTOR_INITIAL_CAPACITY;
+
+  // allocate memory for vector->data
+  vector->data = malloc(sizeof(int) * vector->capacity);
+}
+
+void vector_append(Vector *vector, int value) {
+  // make sure there's room to expand into
+  vector_double_capacity_if_full(vector);
+
+  // append the value and increment vector->size
+  vector->data[vector->size++] = value;
+}
+
+int vector_get(Vector *vector, int index) {
+  if (index >= vector->size || index < 0) {
+    printf("Index %d out of bounds for vector of size %d\n", index, vector->size);
+    exit(1);
+  }
+  return vector->data[index];
+}
+
+void vector_set(Vector *vector, int index, int value) {
+  // zero fill the vector up to the desired index
+  while (index >= vector->size) {
+    vector_append(vector, 0);
+  }
+
+  // set the value at the desired index
+  vector->data[index] = value;
+}
+
+void vector_double_capacity_if_full(Vector *vector) {
+  if (vector->size >= vector->capacity) {
+    // double vector->capacity and resize the allocated memory accordingly
+    vector->capacity *= 2;
+    vector->data = realloc(vector->data, sizeof(int) * vector->capacity);
+  }
+}
+
+void vector_free(Vector *vector) {
+  free(vector->data);
 }
---
+++ vector.h
@@ -0,0 +1,63 @@
+/*
+ * This is a helper lib to implement Vectors (dynamic Arrays) in C
+ */
+
+/*
+ * Constant used for starting capacity of Vectors
+ * Value is arbitrary and can be tuned
+ */
+#define VECTOR_INITIAL_CAPACITY 10
+
+// Define a vector type
+typedef struct {
+  int size;      // slots used so far
+  int capacity;  // total available slots
+  int *data;     // array of integers we're storing
+} Vector;
+
+/*
+ * Function that initializes a vector struct.
+ * It sets size to 0,
+ * capacity to VECTOR_INITIAL_CAPACITY
+ * and allocates an appropriate amount of memory for the underlying data array.
+ */
+void vector_init(Vector *vector);
+
+/*
+ * Appends the given value to the vector.
+ * If the underlying data array is full, then calling this function
+ * should cause vector->data to expand to accept this value.
+ * Increments vector->size.
+ */
+void vector_append(Vector *vector, int value);
+
+/*
+ * Returns a value out of a vector at the given index.
+ * If the index is below 0 or greater than vector->size - 1
+ * this function should complain about the index being out of bounds.
+ */
+int vector_get(Vector *vector, int index);
+
+/*
+ * Sets the value at the given index to the given value.
+ * If the index is greater than the vector->size,
+ * this function should expand the vector until it is big enough to contain
+ * the index and set the value at that index.
+ * It should zero-fill all values in between.
+ * vector->size should be incremented accordingly.
+ */
+void vector_set(Vector *vector, int index, int value);
+
+/*
+ * Doubles the underlying data array capacity
+ * if vector->size >= vector->capacity
+ */
+void vector_double_capacity_if_full(Vector *vector);
+
+/*
+ * Frees the memory allocated for the data array.
+ * We leave freeing of the Vector struct itself to client code
+ * (so they can use any sort of pointer they like,
+ * be it stack or heap, and then clean up after themselves).
+ */
+void vector_free(Vector *vector);
